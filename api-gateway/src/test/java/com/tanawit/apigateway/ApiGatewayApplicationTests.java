afc34c7e79b38b45deb1c8eae7a8e73d1e0d8b77
tanawit.dev <tanawit.dev@hotmail.com>
2020-12-07 17:22:23 +0700

    Implement configuration service and introduct api gateway

---
+++ api-gateway/src/test/java/com/tanawit/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanawit.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
