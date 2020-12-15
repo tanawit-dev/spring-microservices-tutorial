992080888c6622cd6e7562b8db8bda1c376e6dc8
tanawit.dev <tanawit.dev@hotmail.com>
2020-12-15 18:06:26 +0700

    dockerized project

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
-projects="discovery-server config-server api-gateway employee-service department-service organization-service"
+if [ -z "$1" ]
+then
+    projects="discovery-server config-server api-gateway employee-service department-service organization-service"
 
-for project in $projects
-do
-    mvn -f $project/pom.xml clean package -DskipTests
-    docker build -t tanawit17/$project $project/
-done
+    for project in $projects
+    do
+        mvn -f $project/pom.xml clean package -DskipTests
+        docker build -t tanawit17/$project $project/
+    done
+else
+    mvn -f $1/pom.xml clean package -DskipTests
+    docker build -t tanawit17/$1 $1/
+fi
